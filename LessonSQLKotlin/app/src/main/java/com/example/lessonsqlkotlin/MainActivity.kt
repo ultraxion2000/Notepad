8b7419c8704f5bdc7b2764d9bd58d21263d8bded
ultraxion2000 <sasha.bezobrazov@gmail.com>
2022-10-07 00:14:37 +0300

    Создание двух активити + стиль окон

--- LessonSQLKotlin/app/src/main/java/com/example/lessonsqlkotlin/MainActivity.kt
+++ LessonSQLKotlin/app/src/main/java/com/example/lessonsqlkotlin/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.lessonsqlkotlin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -19,25 +20,10 @@ class MainActivity : AppCompatActivity() {
     override fun onResume() {
         super.onResume()
         myDbManager.openDb()
-
-        val TvTest = findViewById<TextView>(R.id.tvTest)
-        val dataList = myDbManager.readDbData()
-        for(item in dataList ){
-            TvTest.append(item)
-            TvTest.append("\n")
-        }
     }
-    fun onClickSave(view: View) {
-        val TvTest = findViewById<TextView>(R.id.tvTest)
-        TvTest.text = ""
-        val EdTitle = findViewById<EditText>(R.id.edTitle)
-        val EdContent = findViewById<EditText>(R.id.edContent)
-        myDbManager.insertToDb(EdTitle.text.toString(), EdContent.text.toString())
-        val dataList = myDbManager.readDbData()
-        for(item in dataList ){
-            TvTest.append(item)
-            TvTest.append("\n")
-        }
+    fun onClickNew(view: View) {
+        val i = Intent(this, EditActivity::class.java )
+        startActivity(i)
     }
 
     override fun onDestroy() {
