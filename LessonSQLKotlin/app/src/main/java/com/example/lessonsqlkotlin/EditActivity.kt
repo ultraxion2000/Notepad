ce471f30dc6c3c0c209e8c415c14a0022d93f8fa
ultraxion2000 <sasha.bezobrazov@gmail.com>
2022-10-07 18:16:16 +0300

    Добавлено поле для редактирования и вставки фото + кнопки

--- LessonSQLKotlin/app/src/main/java/com/example/lessonsqlkotlin/EditActivity.kt
+++ LessonSQLKotlin/app/src/main/java/com/example/lessonsqlkotlin/EditActivity.kt
@@ -1,11 +1,26 @@
 package com.example.lessonsqlkotlin
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.edit_activity.*
 
 class EditActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.edit_activity)
     }
+
+    fun onClickAddImage(view: View) {
+
+        mainImageLayout.visibility = View.VISIBLE
+        fbAddImage.visibility = View.GONE
+
+    }
+
+    fun onClickDeleteImage(view: View) {
+        mainImageLayout.visibility = View.GONE
+        fbAddImage.visibility = View.VISIBLE
+    }
+
 }
