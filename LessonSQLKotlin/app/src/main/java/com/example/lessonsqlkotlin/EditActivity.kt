321bcbc4c4cb99aa9bc876c3482c306f307d8e44
ultraxion2000 <sasha.bezobrazov@gmail.com>
2022-10-08 17:11:11 +0300

    Доббавление данных в SQLite + загрузка фотографий в Notepad

--- LessonSQLKotlin/app/src/main/java/com/example/lessonsqlkotlin/EditActivity.kt
+++ LessonSQLKotlin/app/src/main/java/com/example/lessonsqlkotlin/EditActivity.kt
@@ -1,14 +1,40 @@
 package com.example.lessonsqlkotlin
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import com.example.lessonsqlkotlin.db.MyDbManager
 import kotlinx.android.synthetic.main.edit_activity.*
 
 class EditActivity : AppCompatActivity() {
+
+    val imageRequestCode = 10
+    var tempImageUri = "empty"
+    val myDbManager = MyDbManager(this)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.edit_activity)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        myDbManager.closeDb()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        myDbManager.openDb()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(resultCode == Activity.RESULT_OK && requestCode == imageRequestCode){
+            imMyImage.setImageURI(data?.data)
+            tempImageUri = data?.data.toString()
+        }
     }
 
     fun onClickAddImage(view: View) {
@@ -21,6 +47,21 @@ class EditActivity : AppCompatActivity() {
     fun onClickDeleteImage(view: View) {
         mainImageLayout.visibility = View.GONE
         fbAddImage.visibility = View.VISIBLE
+    }
+
+    fun onClickChooseImage(view: View) {
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        startActivityForResult(intent, imageRequestCode)
+    }
+
+    fun onClickSave(view: View) {
+        val myTitle= edTitle.text.toString()
+        val myDesc = edDesc.text.toString()
+
+        if(myTitle != "" && myDesc != ""){
+            myDbManager.insertToDb(myTitle, myDesc, tempImageUri)
+        }
     }
 
 }
--- LessonSQLKotlin/app/src/main/java/com/example/lessonsqlkotlin/db/MyDbManager.kt
+++ LessonSQLKotlin/app/src/main/java/com/example/lessonsqlkotlin/db/MyDbManager.kt
@@ -13,10 +13,11 @@ class MyDbManager( context: Context) {
         db = myDbHelper.writableDatabase
     }
 
-    fun insertToDb(title: String, content: String) {
+    fun insertToDb(title: String, content: String, uri: String) {
         val values = ContentValues().apply {
             put(MyDbNameClass.COLUMN_NAME_TITLE, title)
             put(MyDbNameClass.COLUMN_NAME_CONTENT, content)
+            put(MyDbNameClass.COLUMN_NAME_IMAGE_URI, uri)
         }
         db?.insert(MyDbNameClass.TABLE_NAME, null, values)
     }
